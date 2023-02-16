58bde496d2f41378e9fa423e401dbff86c44c84c
RomainSav <romain.sv@icloud.com>
2023-02-16 22:18:49 +0100

    UPDATE: base configuration

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+
+    fontFamily: {
+      sans: "Poppins, sans-serif"
+    }
+  },
+  plugins: [],
+}
