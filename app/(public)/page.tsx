f5f4030b6f8fd95e5c603e4d80e69359194a0c98
RomainSav <romain.sv@icloud.com>
2023-02-17 18:38:03 +0100

    UPDATE: add header image

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <div className="h-96 w-full overflow-hidden relative">
+        <div className="absolute z-10 bg-blue-500 h-96 w-full opacity-40" />
+        <img src="/images/bg_img.jpg" alt="" className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- lib/components/navigation/header/header.tsx
+++ lib/components/navigation/header/header.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "@lib/components/navigation/navbar";
 
 export const Header = (): ReactElement => {
     return (
-        <header className="shadow-md">
+        <header className="shadow-md relative z-20">
             <div className="border-b">
                 <div className="container py-2 grid grid-cols-[2fr_1fr] justify-between items-center">
                     <div>
